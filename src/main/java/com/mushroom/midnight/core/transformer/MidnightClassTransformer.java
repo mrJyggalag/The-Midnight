f7fcd222aecc005fc4f9121ce876e1aee9d8d46c
gegy1000 <gegy1000@gmail.com>
2018-10-02 16:21:46 +0200

    OpenAL based reverb

---
+++ src/main/java/com/mushroom/midnight/core/transformer/MidnightClassTransformer.java
@@ -0,0 +1,99 @@
+package com.mushroom.midnight.core.transformer;
+
+import net.minecraft.launchwrapper.IClassTransformer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.FieldInsnNode;
+import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.InsnNode;
+import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.VarInsnNode;
+
+import javax.annotation.Nullable;
+import java.util.ListIterator;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class MidnightClassTransformer implements IClassTransformer, Opcodes {
+    private static final String SOURCE_LWJGL_NAME = "paulscode/sound/libraries/SourceLWJGLOpenAL";
+    private static final String CHANNEL_LWJGL_NAME = "paulscode/sound/libraries/ChannelLWJGLOpenAL";
+
+    private static final Logger LOGGER = LogManager.getLogger(MidnightClassTransformer.class);
+
+    @Override
+    public byte[] transform(String name, String transformedName, byte[] data) {
+        if (data == null) {
+            return null;
+        }
+        if (name.equals("paulscode.sound.libraries.SourceLWJGLOpenAL")) {
+            return this.applyTransform(data, this::transformSoundSource);
+        }
+        return data;
+    }
+
+    private boolean transformSoundSource(ClassNode node) {
+        for (MethodNode method : node.methods) {
+            if (method.name.equals("play")) {
+                this.insertBefore(method.instructions, this.invoke(SOURCE_LWJGL_NAME, "checkPitch"), () -> {
+                    InsnList instructions = new InsnList();
+                    instructions.add(new VarInsnNode(ALOAD, 0));
+                    instructions.add(new FieldInsnNode(GETFIELD, SOURCE_LWJGL_NAME, "channelOpenAL", "L" + CHANNEL_LWJGL_NAME + ";"));
+                    instructions.add(new FieldInsnNode(GETFIELD, CHANNEL_LWJGL_NAME, "ALSource", "Ljava/nio/IntBuffer;"));
+                    instructions.add(new InsnNode(ICONST_0));
+                    instructions.add(new MethodInsnNode(INVOKEVIRTUAL, "java/nio/IntBuffer", "get", "(I)I", false));
+                    instructions.add(new MethodInsnNode(INVOKESTATIC, "com/mushroom/midnight/client/SoundReverbHandler", "onPlaySound", "(I)V", false));
+                    return instructions;
+                });
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private byte[] applyTransform(byte[] data, Predicate<ClassNode> transformer) {
+        ClassNode node = new ClassNode();
+        ClassReader reader = new ClassReader(data);
+        reader.accept(node, 0);
+
+        if (transformer.test(node)) {
+            ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS);
+            node.accept(writer);
+
+            return writer.toByteArray();
+        } else {
+            return data;
+        }
+    }
+
+    private void insertBefore(InsnList instructions, Predicate<AbstractInsnNode> predicate, Supplier<InsnList> insert) {
+        AbstractInsnNode node = this.selectNode(instructions, predicate);
+        if (node != null) {
+            instructions.insertBefore(node, insert.get());
+        } else {
+            StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+            LOGGER.warn("Failed to find location to insert for {}", stackTrace[1].getMethodName());
+        }
+    }
+
+    @Nullable
+    private AbstractInsnNode selectNode(InsnList instructions, Predicate<AbstractInsnNode> predicate) {
+        ListIterator<AbstractInsnNode> iterator = instructions.iterator();
+        while (iterator.hasNext()) {
+            AbstractInsnNode node = iterator.next();
+            if (predicate.test(node)) {
+                return node;
+            }
+        }
+        return null;
+    }
+
+    private Predicate<AbstractInsnNode> invoke(String owner, String name) {
+        return n -> n instanceof MethodInsnNode && ((MethodInsnNode) n).owner.equals(owner) && ((MethodInsnNode) n).name.equals(name);
+    }
+}
