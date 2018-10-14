55bc9ccc46f82ad0c093ece29c781386ce1c9af9
gegy1000 <gegy1000@gmail.com>
2018-10-14 19:54:24 +0200

    Fix broken miasma colors

--- src/main/java/com/mushroom/midnight/common/fluid/FluidMiasma.java
+++ src/main/java/com/mushroom/midnight/common/fluid/FluidMiasma.java
@@ -4,16 +4,16 @@ import com.mushroom.midnight.Midnight;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fluids.Fluid;
 
-import java.awt.Color;
-
 public class FluidMiasma extends Fluid {
     private static final ResourceLocation STILL = new ResourceLocation(Midnight.MODID, "blocks/miasma_still");
     private static final ResourceLocation FLOWING = new ResourceLocation(Midnight.MODID, "blocks/miasma_flow");
 
     public FluidMiasma() {
-        super("miasma", STILL, FLOWING, new Color(206, 69, 216));
+        super("miasma", STILL, FLOWING);
         this.setUnlocalizedName(Midnight.MODID + ".miasma");
         this.setDensity(3000);
         this.setViscosity(3000);
+        this.setLuminosity(15);
+        this.setTemperature(400);
     }
 }
