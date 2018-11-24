636dc70b832a102cc10b628aa8b33189d223357f
arthurbambou <arthur.koalas@free.fr>
2018-11-24 17:54:15 +0100

    Oops

--- src/main/java/com/mushroom/midnight/common/util/MidnightToolMaterials.java
+++ src/main/java/com/mushroom/midnight/common/util/MidnightToolMaterials.java
@@ -3,7 +3,8 @@ package com.mushroom.midnight.common.util;
 import net.minecraft.item.Item.ToolMaterial;
 import net.minecraftforge.common.util.EnumHelper;
 
-public class ToolMaterials{
+public class MidnightToolMaterials {
+    public static final ToolMaterial SHADOWROOT = EnumHelper.addToolMaterial("shadowroot",0,1,1.0F,0.0F,1);
     public static final ToolMaterial TENEBRUM = EnumHelper.addToolMaterial("tenebrum",0,1,1.0F,0.0F,1);
     public static final ToolMaterial NAGRILITE = EnumHelper.addToolMaterial("nagrilite",0,1,1.0F,0.0F,1);
     public static final ToolMaterial EBONYS = EnumHelper.addToolMaterial("ebonys",0,1,1.0F,0.0F,1);
