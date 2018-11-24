a6ed5c29fd86d560257e5794d8793e4a1df04bd0
arthurbambou <arthur.koalas@free.fr>
2018-11-24 18:42:17 +0100

    Add Midnight Tools Creative Tab

--- src/main/java/com/mushroom/midnight/common/item/tools/ItemMidnightPickaxe.java
+++ src/main/java/com/mushroom/midnight/common/item/tools/ItemMidnightPickaxe.java
@@ -2,10 +2,11 @@ package com.mushroom.midnight.common.item.tools;
 
 import com.mushroom.midnight.Midnight;
 import com.mushroom.midnight.client.IModelProvider;
+import net.minecraft.item.ItemPickaxe;
 
-public class ItemMidnightPickaxe extends net.minecraft.item.ItemPickaxe implements IModelProvider {
+public class ItemMidnightPickaxe extends ItemPickaxe implements IModelProvider {
     public ItemMidnightPickaxe(ToolMaterial material) {
         super(material);
-        this.setCreativeTab(Midnight.MIDNIGHT_ITEMS);
+        this.setCreativeTab(Midnight.MIDNIGHT_TOOLS);
     }
 }
