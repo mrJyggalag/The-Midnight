2520d4dd9578fa579017b4efee5dcc7bd19cdf49
arthurbambou <arthur.koalas@free.fr>
2018-11-24 17:35:11 +0100

    Add Tool Materials (Changes in values will be required)

---
+++ src/main/java/com/mushroom/midnight/common/util/ToolMaterials.java
@@ -0,0 +1,11 @@
+package com.mushroom.midnight.common.util;
+
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraftforge.common.util.EnumHelper;
+
+public class ToolMaterials{
+    public static final ToolMaterial TENEBRUM = EnumHelper.addToolMaterial("tenebrum",0,1,1.0F,0.0F,1);
+    public static final ToolMaterial NAGRILITE = EnumHelper.addToolMaterial("nagrilite",0,1,1.0F,0.0F,1);
+    public static final ToolMaterial EBONYS = EnumHelper.addToolMaterial("ebonys",0,1,1.0F,0.0F,1);
+    public static final ToolMaterial ROCKSHROOM = EnumHelper.addToolMaterial("rockshroom",0,1,1.0F,0.0F,1);
+}
