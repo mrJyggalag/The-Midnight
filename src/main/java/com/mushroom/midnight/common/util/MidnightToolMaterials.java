a3cc9bfa8f0b9019ba510f60ba8456fb79d285d7
Cipher Zero X <cipherzerox@gmail.com>
2018-11-24 11:17:46 -0600

    Fixed tool material values

--- src/main/java/com/mushroom/midnight/common/util/MidnightToolMaterials.java
+++ src/main/java/com/mushroom/midnight/common/util/MidnightToolMaterials.java
@@ -4,9 +4,9 @@ import net.minecraft.item.Item.ToolMaterial;
 import net.minecraftforge.common.util.EnumHelper;
 
 public class MidnightToolMaterials {
-    public static final ToolMaterial SHADOWROOT = EnumHelper.addToolMaterial("shadowroot",0,1,1.0F,0.0F,1);
-    public static final ToolMaterial TENEBRUM = EnumHelper.addToolMaterial("tenebrum",0,1,1.0F,0.0F,1);
-    public static final ToolMaterial NAGRILITE = EnumHelper.addToolMaterial("nagrilite",0,1,1.0F,0.0F,1);
-    public static final ToolMaterial EBONYS = EnumHelper.addToolMaterial("ebonys",0,1,1.0F,0.0F,1);
-    public static final ToolMaterial ROCKSHROOM = EnumHelper.addToolMaterial("rockshroom",0,1,1.0F,0.0F,1);
+    public static final ToolMaterial SHADOWROOT = EnumHelper.addToolMaterial("shadowroot",0, 59, 2.0F, 0.0F, 14);
+    public static final ToolMaterial NIGHTSTONE = EnumHelper.addToolMaterial("nightstone",1, 150, 4.0F, 1.0F, 5);
+    public static final ToolMaterial EBONYS = EnumHelper.addToolMaterial("ebonys",2, 350, 6.0F, 2.0F, 15);
+    public static final ToolMaterial NAGRILITE = EnumHelper.addToolMaterial("nagrilite",3, 850, 3.0F,3.0F,10);
+    public static final ToolMaterial TENEBRUM = EnumHelper.addToolMaterial("tenebrum",4, 1561, 10.0F, 4.0F,22);
 }
