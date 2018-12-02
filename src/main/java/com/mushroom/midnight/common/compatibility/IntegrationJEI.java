83d8dfb11b9b624917423d7c3cd094318911a52c
Corail31 <m.pietquin@free.fr>
2018-12-02 11:45:02 +0100

    blacklist items in JEI
    mainly for the blocks fluid but could be used for advancement icons, or anvil recipes...

---
+++ src/main/java/com/mushroom/midnight/common/compatibility/IntegrationJEI.java
@@ -0,0 +1,21 @@
+package com.mushroom.midnight.common.compatibility;
+
+import com.mushroom.midnight.common.registry.ModBlocks;
+import mezz.jei.api.IJeiHelpers;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.IModRegistry;
+import mezz.jei.api.JEIPlugin;
+import mezz.jei.api.ingredients.IIngredientBlacklist;
+import net.minecraft.item.ItemStack;
+
+@JEIPlugin
+public class IntegrationJEI implements IModPlugin {
+	@Override
+	public void register(IModRegistry registry) {
+		IJeiHelpers jeiHelpers = registry.getJeiHelpers();
+		// blacklisted items
+		IIngredientBlacklist blackList = jeiHelpers.getIngredientBlacklist();
+		blackList.addIngredientToBlacklist(new ItemStack(ModBlocks.DARK_WATER));
+		blackList.addIngredientToBlacklist(new ItemStack(ModBlocks.MIASMA));
+	}
+}
