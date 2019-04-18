36f89fb46c835ba69bbb6be9f140575d62f6b07e
five0five <44749261+five0five@users.noreply.github.com>
2019-04-18 23:04:43 +0100

    applied config lang changes to .cfg

--- src/main/java/com/mushroom/midnight/common/config/MidnightConfig.java
+++ src/main/java/com/mushroom/midnight/common/config/MidnightConfig.java
@@ -24,86 +24,86 @@ public class MidnightConfig {
     public static class CatGeneral {
         @Config.Name("midnight_dimension_id")
         @Config.LangKey("config.midnight.midnight_dimension_id")
-        @Config.Comment("The ID to be used for the Midnight Dimension")
+        @Config.Comment("The ID to be used for the Midnight Dimension. Default= -23")
         @Config.RequiresMcRestart
         public int midnightDimensionId = -23;
 
         @Config.Name("bladeshroom_damage_chance")
         @Config.LangKey("config.midnight.bladeshroom_damage_chance")
-        @Config.Comment("The chance to take damage when picking a bladeshroom cap.")
+        @Config.Comment("The chance to take a small amount of damage when picking a bladeshroom cap as a percentage. If 0, this is disabled. Default= 5")
         @Config.RangeInt(min=0, max=100)
         public int bladeshroomDamageChance = 5;
 
         @Config.Name("can_respawn_in_midnight")
         @Config.LangKey("config.midnight.can_respawn_in_midnight")
-        @Config.Comment("If true, players will be allowed to respawn in Midnight.")
+        @Config.Comment("If true, players will respawn in Midnight after dying in the dimension. Default= false")
         public boolean canRespawnInMidnight = false;
 
         @Config.Name("allow_lightning_damage")
         @Config.LangKey("config.midnight.allow_lightning_damage")
-        @Config.Comment("Allows the lightnings in Midnight to burn the lands and do damage to entities.")
+        @Config.Comment("Allows the lightning in Midnight to burn blocks and do damage to entities. Default= false")
         public boolean allowLightningDamage = false;
 
         @Config.Name("rift_spawn_rarity")
         @Config.LangKey("config.midnight.rift_spawn_rarity")
-        @Config.Comment("The rarity that a rift appears. Larger numbers increase rarity. If 0, rifts don't spawn.")
+        @Config.Comment("The rarity that a rift appears. Larger numbers increase rarity. If 0, rifts don't spawn. Default= 1000")
         @Config.RangeInt(min=0, max=10000)
         public int riftSpawnRarity = 1000;
 
         @Config.Name("rifter_spawn_rarity")
         @Config.LangKey("config.midnight.rifter_spawn_rarity")
-        @Config.Comment("The rarity that rifts spawn rifters. Larger numbers increase rarity. If 0, rifters don't spawn.")
+        @Config.Comment("The rarity that rifts spawn rifters. Larger numbers increase rarity. If 0, rifters don't spawn. Default= 1000")
         @Config.RangeInt(min=0, max=10000)
         public int rifterSpawnRarity = 1000;
 
         @Config.Name("max_rifter_by_rift")
         @Config.LangKey("config.midnight.max_rifter_by_rift")
-        @Config.Comment("The maximum of rifters spawning from a rift.")
+        @Config.Comment("The maximum amount of rifters that spawn from a rift. Default= 1")
         @Config.RangeInt(min=1, max=5)
         public int maxRifterByRift = 1;
 
         @Config.Name("allow_rifter_teleport")
         @Config.LangKey("config.midnight.allow_rifter_teleport")
-        @Config.Comment("Allows the rifters to teleport on players.")
+        @Config.Comment("Allows rifters to teleport to players when they aren't being looked at. Default= true")
         public boolean allowRifterTeleport = true;
 
         @Config.Name("monster_spawn_rate")
         @Config.LangKey("config.midnight.monster_spawn_rate")
-        @Config.Comment("Spawn rate of creatures of type monster.")
+        @Config.Comment("Spawn rate of entities of the 'monster' type in midnight (eg. Rifter, Hunter). Default= 400")
         @Config.RangeInt(min=1)
         public int monsterSpawnRate = 400;
 
         @Config.Name("rifter_capture_tamed_animal")
         @Config.LangKey("config.midnight.rifter_capture_tamed_animal")
-        @Config.Comment("Allows the rifters to capture tamed animals.")
+        @Config.Comment("Allows rifters to capture tamed entities and drag them into rifts. Default= false")
         public boolean rifterCaptureTamedAnimal = false;
 
         @Config.Name("capturable_entities")
         @Config.RequiresWorldRestart
         @Config.LangKey("config.midnight." + "capturable_entities")
-        @Config.Comment("List of entities(others than player and animal) capturable by rifters.")
+        @Config.Comment("Mobs on this whitelist can be captured by rifters, players and animals are already captured by default. Format is mod id:name of creature, just use the modid to whitelist everything from it. Default= empty")
         public String[] capturableEntities = {
         };
 
         @Config.Name("not_capturable_animals")
         @Config.RequiresWorldRestart
         @Config.LangKey("config.midnight." + "not_capturable_animals")
-        @Config.Comment("List of animals not capturable by rifters.")
+        @Config.Comment("Mobs on this blacklist cannot be captured by rifters. Format is mod id:name of creature, just use the mod id to blacklist everything from it. Default= "iceandfire", "midnight:nightstag"")
         public String[] notCapturableAnimals = {
                 "iceandfire",
-                "midnight:nighstag"
+                "midnight:nightstag"
         };
     }
 
     public static class CatClient {
         @Config.Name("hide_vignette_effect")
         @Config.LangKey("config.midnight.hide_vignette_effect")
-        @Config.Comment("Hides the vignette effect in the dark areas of Midnight.")
+        @Config.Comment("Hides the vignette effect in the darker areas of Midnight. Default= true")
         public boolean hideVignetteEffect = true;
 
         @Config.Name("rift_shaders")
         @Config.LangKey("config.midnight.rift_shaders")
-        @Config.Comment("If true, rifts will be rendered with custom shaders for standard effects. If false, a simpler texture will be rendered.")
+        @Config.Comment("If true, rifts will be rendered with custom shaders for standard effects. If false, a simpler (motionless) texture will be rendered. Default= true")
         public boolean riftShaders = true;
     }
 
