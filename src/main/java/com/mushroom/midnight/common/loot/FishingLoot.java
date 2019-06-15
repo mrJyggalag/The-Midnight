845727f119729c96f3be17a36366758229bb3303
Corail31 <m.pietquin@free.fr>
2019-06-15 11:01:05 +0200

    apply fishing hooks to all main fishing tables
    also avoid a crash with Better with Mods (where the pool "main" is not present is the fishing loot table)

--- src/main/java/com/mushroom/midnight/common/loot/FishingLoot.java
+++ src/main/java/com/mushroom/midnight/common/loot/FishingLoot.java
@@ -15,28 +15,50 @@ import net.minecraftforge.fml.relauncher.ReflectionHelper;
 import java.lang.reflect.Field;
 import java.util.List;
 
+import static com.mushroom.midnight.Midnight.LOGGER;
 import static com.mushroom.midnight.Midnight.MODID;
 
 @Mod.EventBusSubscriber(modid = MODID)
 public class FishingLoot {
+    private static final Field fieldPools;
     private static final Field poolConditions;
     static {
+        fieldPools = ReflectionHelper.findField(LootTable.class, "pools", "field_186466_c");
         poolConditions = ReflectionHelper.findField(LootPool.class, "poolConditions", "field_186454_b");
-        poolConditions.setAccessible(true);
     }
 
     @SubscribeEvent(priority= EventPriority.LOWEST)
     public static void onLootTableLoad(LootTableLoadEvent event) {
         if (event.getName() == LootTableList.GAMEPLAY_FISHING) {
-            addConditionToPool(event.getTable(), "main", (rand, context) -> !Helper.isMidnightDimension(context.getWorld()));
-            event.getTable().addPool(event.getLootTableManager().getLootTableFromLocation(new ResourceLocation(MODID,"fishing")).getPool("midnight_fishing"));
-            addConditionToPool(event.getTable(), "midnight_fishing", (rand, context) -> Helper.isMidnightDimension(context.getWorld()));
+            addConditionToAllMainPools(event.getTable(), (rand, context) -> !Helper.isMidnightDimension(context.getWorld()));
+            LootTable midnightTable = event.getLootTableManager().getLootTableFromLocation(new ResourceLocation(MODID, "fishing"));
+            if (midnightTable == null) {
+                LOGGER.warn("The Midnight fishing loottable is absent");
+            } else {
+                LootPool midnightPool = midnightTable.getPool("midnight_fishing");
+                event.getTable().addPool(midnightPool);
+                addConditionToPool((rand, context) -> Helper.isMidnightDimension(context.getWorld()), midnightPool);
+            }
         }
     }
 
     @SuppressWarnings("unchecked")
-    private static void addConditionToPool(LootTable table, String poolName, LootCondition condition) {
-        LootPool pool = table.getPool(poolName);
+    private static void addConditionToAllMainPools(LootTable table, LootCondition condition) {
+        List<LootPool> pools = null;
+        try {
+            pools = (List<LootPool>) fieldPools.get(table);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        if (pools != null) {
+            for (LootPool pool : pools) {
+                addConditionToPool(condition, pool);
+            }
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static void addConditionToPool(LootCondition condition, LootPool pool) {
         try {
             List<LootCondition> conditions = (List<LootCondition>) poolConditions.get(pool);
             conditions.add(condition);
