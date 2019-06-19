b177cc0da9092249e3e0eed879eb14bfc51e7bb4
Corail31 <m.pietquin@free.fr>
2019-06-19 13:26:37 +0200

    fix #239

--- src/main/java/com/mushroom/midnight/client/particle/MidnightParticles.java
+++ src/main/java/com/mushroom/midnight/client/particle/MidnightParticles.java
@@ -5,35 +5,58 @@ import net.minecraft.client.particle.IParticleFactory;
 import net.minecraft.client.particle.Particle;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nullable;
 
 public enum MidnightParticles {
-    AMBIENT_SPORE(new AmbientSporeParticle.Factory()),
-    SPORE(new SporeParticle.Factory()),
-    DRIP(new DripParticle.Factory()),
-    UNSTABLE_BUSH(new UnstableBushParticle.Factory()),
-    SPORCH(new SporchParticle.Factory()),
-    FURNACE_FLAME(new FurnaceFlameParticle.Factory()),
-    BOMB_EXPLOSION(new BombExplosionParticle.Factory()),
-    FADING_SPORE(new FadingSporeParticle.Factory());
+    AMBIENT_SPORE, SPORE, DRIP, UNSTABLE_BUSH, SPORCH, FURNACE_FLAME, BOMB_EXPLOSION, FADING_SPORE;
 
-    IParticleFactory factory;
-
-    MidnightParticles(IParticleFactory factory) {
-        this.factory = factory;
+    MidnightParticles() {
     }
 
+    @SideOnly(Side.CLIENT)
+    @Nullable
     public Particle create(World world, double x, double y, double z, double velocityX, double velocityY, double velocityZ, int... parameters) {
-        return this.factory.createParticle(-1, world, x, y, z, velocityX, velocityY, velocityZ, parameters);
+        IParticleFactory factory = getFactory();
+        return factory == null ? null : factory.createParticle(-1, world, x, y, z, velocityX, velocityY, velocityZ, parameters);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Nullable
+    public IParticleFactory getFactory() {
+        switch (this) {
+            case AMBIENT_SPORE:
+                return new AmbientSporeParticle.Factory();
+            case SPORE:
+                return new SporeParticle.Factory();
+            case DRIP:
+                return new DripParticle.Factory();
+            case UNSTABLE_BUSH:
+                return new UnstableBushParticle.Factory();
+            case SPORCH:
+                return new SporchParticle.Factory();
+            case FURNACE_FLAME:
+                return new FurnaceFlameParticle.Factory();
+            case BOMB_EXPLOSION:
+                return new BombExplosionParticle.Factory();
+            case FADING_SPORE:
+                return new FadingSporeParticle.Factory();
+        }
+        return null;
     }
 
     public void spawn(World world, double x, double y, double z, double velocityX, double velocityY, double velocityZ, int... parameters) {
-        if (!world.isRemote) {
-            return;
+        if (world.isRemote) {
+            Particle particle = create(world, x, y, z, velocityX, velocityY, velocityZ, parameters);
+            if (particle != null) {
+                spawn(particle);
+            }
         }
-        Particle particle = this.create(world, x, y, z, velocityX, velocityY, velocityZ, parameters);
-        this.spawn(particle);
     }
 
+    @SideOnly(Side.CLIENT)
     private void spawn(Particle particle) {
         Minecraft.getMinecraft().effectRenderer.addEffect(particle);
     }
