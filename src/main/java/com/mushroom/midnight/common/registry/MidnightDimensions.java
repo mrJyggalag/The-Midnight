0f1f77708c5f581ed7d201ebd915e2a6c74eb51b
gegy1000 <gegy1000@gmail.com>
2019-07-02 14:09:16 +0200

    Fix crash in debug menu

--- src/main/java/com/mushroom/midnight/common/registry/MidnightDimensions.java
+++ src/main/java/com/mushroom/midnight/common/registry/MidnightDimensions.java
@@ -3,6 +3,7 @@ package com.mushroom.midnight.common.registry;
 import com.google.common.base.Preconditions;
 import com.mushroom.midnight.Midnight;
 import com.mushroom.midnight.common.world.MidnightDimension;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.Dimension;
 import net.minecraft.world.dimension.DimensionType;
@@ -17,8 +18,6 @@ import java.util.function.BiFunction;
 
 @Mod.EventBusSubscriber(modid = Midnight.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class MidnightDimensions {
-    private static DimensionType midnightDimension;
-
     @SubscribeEvent
     public static void registerModDimensions(RegistryEvent.Register<ModDimension> event) {
         ModDimension midnight = new ModDimension() {
@@ -31,12 +30,13 @@ public class MidnightDimensions {
         RegUtil.generic(event.getRegistry())
                 .add("midnight", midnight);
 
-        midnightDimension = DimensionManager.registerDimension(midnight.getRegistryName(), midnight, null, false);
+        DimensionManager.registerDimension(midnight.getRegistryName(), midnight, null, false);
     }
 
     @Nonnull
     public static DimensionType midnight() {
-        Preconditions.checkNotNull(midnightDimension, "dimension not yet initialized");
-        return midnightDimension;
+        DimensionType dimension = DimensionType.byName(new ResourceLocation(Midnight.MODID, "midnight"));
+        Preconditions.checkNotNull(dimension, "dimension not yet initialized");
+        return dimension;
     }
 }
