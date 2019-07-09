8fc795fbfd41bf527ac1e245e4360205bfa1f871
gegy1000 <gegy1000@gmail.com>
2019-07-09 10:59:07 +0200

    Fix midnight chest render bounds

--- src/main/java/com/mushroom/midnight/common/tile/base/MidnightChestTileEntity.java
+++ src/main/java/com/mushroom/midnight/common/tile/base/MidnightChestTileEntity.java
@@ -3,6 +3,7 @@ package com.mushroom.midnight.common.tile.base;
 import com.mushroom.midnight.common.registry.MidnightTileEntities;
 import net.minecraft.tileentity.ChestTileEntity;
 import net.minecraft.tileentity.TileEntityType;
+import net.minecraft.util.math.AxisAlignedBB;
 
 public class MidnightChestTileEntity extends ChestTileEntity {
     protected MidnightChestTileEntity(TileEntityType<?> entityType) {
@@ -11,5 +12,13 @@ public class MidnightChestTileEntity extends ChestTileEntity {
 
     public MidnightChestTileEntity() {
         this(MidnightTileEntities.MIDNIGHT_CHEST);
+    }
+
+    @Override
+    public AxisAlignedBB getRenderBoundingBox() {
+        return new AxisAlignedBB(
+                this.pos.add(-1, 0, -1),
+                this.pos.add(2, 2, 2)
+        );
     }
 }
