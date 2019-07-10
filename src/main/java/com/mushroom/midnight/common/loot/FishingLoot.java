f327f8f6fe73577ebc63549be446e3c623da8ddc
Corail31 <m.pietquin@free.fr>
2019-07-10 19:07:05 +0200

    fishing loot tables, round 2

--- src/main/java/com/mushroom/midnight/common/loot/FishingLoot.java
+++ src/main/java/com/mushroom/midnight/common/loot/FishingLoot.java
@@ -8,6 +8,7 @@ import net.minecraft.world.storage.loot.LootPool;
 import net.minecraft.world.storage.loot.LootTable;
 import net.minecraft.world.storage.loot.LootTables;
 import net.minecraft.world.storage.loot.conditions.ILootCondition;
+import net.minecraft.world.storage.loot.conditions.LootConditionManager;
 import net.minecraftforge.event.LootTableLoadEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -25,11 +26,13 @@ import static com.mushroom.midnight.Midnight.MODID;
 public class FishingLoot {
     private static ResourceLocation MIDNIGHT_FISHING = new ResourceLocation(MODID, "fishing");
     private static final Field fieldIsFrozen = ObfuscationReflectionHelper.findField(LootTable.class, "isFrozen");
+    private static final Field fieldPools = ObfuscationReflectionHelper.findField(LootTable.class, "field_186466_c");
+    private static final Field fieldConditions = ObfuscationReflectionHelper.findField(LootPool.class, "field_186454_b");
+    private static final Field fieldCombinedConditions = ObfuscationReflectionHelper.findField(LootPool.class, "field_216101_c");
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public static void onLootTableLoad(LootTableLoadEvent event) {
         if (event.getName().equals(LootTables.GAMEPLAY_FISHING)) {
-            addConditionToAllMainPools(event.getTable(), context -> !Helper.isMidnightDimension(context.getWorld()));
             MinecraftServer server = LogicalSidedProvider.INSTANCE.get(LogicalSide.SERVER);
             server.deferTask(() -> {
                 LootTable midnightFishing = event.getLootTableManager().getLootTableFromLocation(MIDNIGHT_FISHING);
@@ -40,10 +43,11 @@ public class FishingLoot {
                         // because modded loot tables are not fired here
                         fieldIsFrozen.setBoolean(midnightFishing, false);
                         fieldIsFrozen.setBoolean(event.getTable(), false);
+                        addConditionToAllMainPools(event.getTable(), context -> !Helper.isMidnightDimension(context.getWorld()));
 
                         LootPool midnightPool = midnightFishing.getPool("midnight_fishing");
-                        event.getTable().addPool(midnightPool);
                         addConditionToPool(context -> Helper.isMidnightDimension(context.getWorld()), midnightPool);
+                        event.getTable().addPool(midnightPool);
 
                         fieldIsFrozen.setBoolean(event.getTable(), true);
                         fieldIsFrozen.setBoolean(midnightFishing, true);
@@ -56,9 +60,9 @@ public class FishingLoot {
     }
 
     @SuppressWarnings("unchecked")
-    private static void addConditionToAllMainPools(LootTable table, ILootCondition condition) {
+    private static void addConditionToAllMainPools(LootTable table, ILootCondition condition) throws IllegalAccessException {
         List<LootPool> pools = null;
-        pools = ObfuscationReflectionHelper.getPrivateValue(LootTable.class, table, "field_186466_c");
+        pools = (List<LootPool>) fieldPools.get(table);
         if (pools != null) {
             for (LootPool pool : pools) {
                 addConditionToPool(condition, pool);
@@ -67,8 +71,9 @@ public class FishingLoot {
     }
 
     @SuppressWarnings("unchecked")
-    private static void addConditionToPool(ILootCondition condition, LootPool pool) {
-        List<ILootCondition> conditions = ObfuscationReflectionHelper.getPrivateValue(LootPool.class, pool, "field_186454_b");
+    private static void addConditionToPool(ILootCondition condition, LootPool pool) throws IllegalAccessException {
+        List<ILootCondition> conditions = (List<ILootCondition>) fieldConditions.get(pool);
         conditions.add(condition);
+        fieldCombinedConditions.set(pool, LootConditionManager.and(conditions.toArray(new ILootCondition[0])));
     }
 }
